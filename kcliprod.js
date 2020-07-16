049fc0a96291cdfc3eb64f208aefba0c45157a20
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-07-16 19:56:07 +0530

    add ddb

--- kcliprod.js
+++ kcliprod.js
@@ -1,4 +1,15 @@
+const AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+
 exports.handler = async (event) => {
+    try {
+        let data = await ddb.scan({
+    TableName: "kkk"
+    }).promise();
+            
+    } catch (err) {
+        // error handling goes here
+    };
     
     return {"message": "Successfully executed"};
 };
